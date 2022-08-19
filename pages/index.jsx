b113a3ce26a45a53ade930f6181833a8e6229865
Joaquin Repetto <joaquinrepetto@MacBook-Pro-de-Joaquin.local>
2022-08-19 20:48:34 -0300

    deposit function

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,65 @@
+import { useState } from "react";
+import { useProvider, useContract, useSigner, useAccount } from "wagmi";
 import Card from "../components/Card";
-import abi from "../contracts/poolTogetherV3ABI.json";
-import { useProvider } from "wagmi";
-
+import abi from "../contracts/PrizePool.json";
+import erc20abi from "../contracts/ERC20ABI.json";
 import Layout from "../components/Layout";
 import { ethers } from "ethers";
 
 export default function Home() {
-  const provider = useProvider();
+  const [amount, setAmount] = useState();
+  // const provider = useProvider();
+  // const { address, isConnected } = useAccount();
+
+  const { data: signer } = useSigner();
+
+  // Mumbai PrizePool contract address
+  const mumbaiContractAddress = "0x26e507ce8ce828abc7fc986c8a19f4354d5b8cd2";
+
+  // AAVE Mumbai token address
+  const controlledToken = "0x74c873c719866a650F151A9fBF0cB5b5c6315906";
+
+  const wooyPoolContract = useContract({
+    addressOrName: mumbaiContractAddress,
+    contractInterface: abi,
+    signerOrProvider: signer,
+  });
+
+  const mumbaiAaveContract = useContract({
+    addressOrName: controlledToken,
+    contractInterface: erc20abi,
+    signerOrProvider: signer,
+  });
 
   const deposit = async () => {
-    const mumbaiContractAddres = "0x26e507ce8ce828abc7fc986c8a19f4354d5b8cd2";
-    const wooyPoolContract = new ethers.Contract(
-      mumbaiContractAddres,
-      abi,
-      provider
+    // const wooyPoolContract = new ethers.Contract(
+    //   mumbaiContractAddress,
+    //   abi,
+    //   provider
+    // );
+    const address = await signer.getAddress();
+
+    const parsedAmount = ethers.utils.parseUnits(amount);
+
+    const approveTx = await mumbaiAaveContract.approve(
+      mumbaiContractAddress,
+      parsedAmount
+    );
+    console.log(approveTx, "aprobacion");
+    const aTx = await approveTx.wait();
+    console.log(aTx, "await tx");
+
+    const tx = await wooyPoolContract.depositTo(
+      address,
+      parsedAmount,
+      controlledToken,
+      ethers.constants.AddressZero
     );
 
-    const tx = await wooyPoolContract.deposit();
+    console.log(tx, "Depositando....");
 
-    console.log(tx, "asd");
+    const depositAtx = await tx.wait();
+    console.log(depositAtx, "depositAtx");
   };
   return (
     <Layout>
@@ -31,6 +72,11 @@ export default function Home() {
             desc={"This will be the best prize of the world"}
             buttonlabel={"Enter the pool"}
             path="/"
+          />
+          <input
+            placeholder="Ingresar monto a depositar"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
           />
           <button style={{ backgroundColor: "red" }} onClick={deposit}>
             Join the pool
