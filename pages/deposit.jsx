2c8098b46ca9b30d1fda59be658b672ce71a357a
Cancu <juancruz.cancu@gmail.com>
2022-08-22 03:54:00 -0300

    deposit finished

--- pages/deposit.jsx
+++ pages/deposit.jsx
@@ -7,6 +7,7 @@ import erc20abi from '../contracts/ERC20ABI.json'
 import ControlledTokenAbi from '../contracts/ControlledToken.json'
 import { ethers } from 'ethers'
 // import { useSendTransactionWrapper } from "../hooks/useSendTransactionWrapper";
+import { CircularProgress } from '@mui/material'
 
 const deposit = () => {
 	const { data: signer } = useSigner()
@@ -39,6 +40,9 @@ const deposit = () => {
 		signerOrProvider: signer,
 	})
 
+	const [isLoading, setIsLoading] = useState(false)
+	const [txFinished, setTxFinished] = useState(false)
+
 	const deposit = async (e) => {
 		const address = await signer.getAddress()
 		const parsedAmount = ethers.utils.parseUnits(amount)
@@ -46,49 +50,68 @@ const deposit = () => {
 
 		if (!parsedAmount) return
 
-		const approveTx = await erc20Contract.approve(poolAddress, parsedAmount)
-		console.log(approveTx, 'aprobacion')
-		const aTx = await approveTx.wait()
-		console.log(aTx, 'await tx')
+		setIsLoading(true)
 
-		const tx = await wooyPoolContract.depositTo(
-			address,
-			parsedAmount,
-			tokenAddress,
-			ethers.constants.AddressZero,
-			{ gasLimit: 1000000 }
-		)
-
-		console.log(tx, 'Depositando....')
-
-		const depositAtx = await tx.wait()
-		console.log(depositAtx, 'depositAtx')
+		const approveTx = await erc20Contract
+			.approve(poolAddress, parsedAmount)
+			.then(() => {
+				const tx = wooyPoolContract
+					.depositTo(
+						address,
+						parsedAmount,
+						tokenAddress,
+						ethers.constants.AddressZero,
+						{ gasLimit: 1000000 }
+					)
+					.then(() => {
+						setIsLoading(false)
+					})
+					.finally(() => {
+						setIsLoading(true)
+					})
+			})
 	}
 
 	return (
 		<Layout>
 			<div className="flex justify-center items-center gap-8 h-full w-full">
 				<div className="bg-white h-full flex flex-col rounded-md py-24 px-48">
-					<h1 className="text-black">Deposit AAVE on Polygon</h1>
-					<p className="text-black">
-						More networks and tokens coming soon!
-					</p>
-					<input
-						type="text"
-						placeholder="Enter Amount!"
-						className="border-2 rounded-lg px-4 py-2 border-black mt-8"
-						onChange={(e) => setAmount(e.target.value)}
-					/>
-					<button
-						onClick={() => deposit()}
-						className="mt-4 lg:px-16 px-8 py-2 bg-[#ED652B] rounded-md hover:shadow-2xl hover:bg-[#fd692a] transition-all"
-					>
-						CONTINUE
-					</button>
-					<p className="text-black">
-						You'll need to approve this transaction on your wallet
-						first.
-					</p>
+					{txFinished ? (
+						<h1>🎉 Your deposit was successful! 🎉</h1>
+					) : (
+						<>
+							{isLoading ? (
+								<CircularProgress />
+							) : (
+								<>
+									<h1 className="text-black">
+										Deposit AAVE on Polygon
+									</h1>
+									<p className="text-black">
+										More networks and tokens coming soon!
+									</p>
+									<input
+										type="text"
+										placeholder="Enter Amount!"
+										className="border-2 rounded-lg px-4 py-2 border-black mt-8"
+										onChange={(e) =>
+											setAmount(e.target.value)
+										}
+									/>
+									<button
+										onClick={() => deposit()}
+										className="mt-4 lg:px-16 px-8 py-2 bg-[#ED652B] rounded-md hover:shadow-2xl hover:bg-[#fd692a] transition-all"
+									>
+										DEPOSIT
+									</button>
+									<p className="text-black">
+										You'll need to approve this transaction
+										on your wallet first.
+									</p>
+								</>
+							)}
+						</>
+					)}
 				</div>
 			</div>
 		</Layout>
